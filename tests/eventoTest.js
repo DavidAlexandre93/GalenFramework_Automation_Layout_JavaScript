922ad2803aa75b9189e0815182b4c386dc1f1c03
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2019-01-13 20:56:33 -0200

    TestLayout

---
+++ tests/eventoTest.js
@@ -0,0 +1,30 @@
+
+/*this.WelcomePage = function (driver) {   
+    WelcomePage.loginButton.hover();
+    }*/
+
+    //this.WelcomePage = $page("Welcome page", {
+    //    loginButton: " .button-login"
+    // });
+    //var welcomePage = new WelcomePage(driver);
+   
+    
+
+    //this.MyNotesPage = function (driver) {
+        //GalenPages.extendPage(this, driver, {
+      this.MyNotesPage = $page("MyNotesPage", {
+          submitButton: " .btn-primary" // id local da pagina
+        });
+      
+      
+    var myNotesPage = new MyNotesPage(driver);
+    myNotesPage.submitButton.hover();
+    //myNotesPage.hoverbtn_primary();
+
+    //this.MyNotesPage = function (driver) {
+        //myNotesPage.hoversubmitButton();
+        
+
+       // GalenPages.sleep(3000);
+   // }
+
